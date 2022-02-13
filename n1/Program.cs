2e398761da18376e318b5dd6f19fb816bce8834e
wdkokskwpqo <a.vengrzhinovskaya@gmail.com>
2022-02-13 19:36:21 +0700

    Add project files.

---
+++ n1/Program.cs
@@ -0,0 +1,47 @@
+﻿/******************************************
+ * Автор: Венгржиновская А. С.            *
+ * Дата: 07.02.2022                       *
+ * Название: Базовые понятия языка С# - 1 *
+*******************************************/
+
+using System;
+
+namespace n1 {
+
+	class Program {
+
+		static void Main(string[] args) {
+
+            Console.WriteLine("Enter a:");
+			int Number = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter n:");
+			int Exponent = int.Parse(Console.ReadLine());
+
+			bool Negative = false;
+
+			if (Exponent < 0) {
+
+				Negative = true;
+				Exponent *= -1;
+            }
+
+			int Result = 1;
+
+			for (int ExponentIndex = 1; ExponentIndex <= Exponent; ++ExponentIndex) {
+
+				Result *= Number;
+            }
+
+			if (Negative) {
+
+                Console.WriteLine("\n" + "Result: 1/" + Result);
+            } else {
+
+				Console.WriteLine("\n" + "Result: " + Result);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ n2/Program.cs
@@ -0,0 +1,38 @@
+﻿/******************************************
+ * Автор: Венгржиновская А. С.            *
+ * Дата: 07.02.2022                       *
+ * Название: Базовые понятия языка С# - 2 *
+*******************************************/
+
+using System;
+
+namespace n2 {
+
+	class Program {
+
+		static void Main(string[] args) {
+
+			Console.WriteLine("Enter x:");
+			var Number = Console.ReadLine();
+
+			int NumberInt;
+
+			if (!int.TryParse(Number, out NumberInt) || NumberInt < 100) {
+
+                Console.WriteLine("\n" + "Invalid number");
+
+				return;
+			}
+
+			Console.Clear();
+
+			Console.WriteLine("x: " + Number);
+
+			Number = Number.Remove(1, 1) + Number.Substring(1, Number.Length - (Number.Length - 1));
+
+			Console.WriteLine("n: " + Number);
+
+			Console.ReadKey();
+		}
+	}
+}
